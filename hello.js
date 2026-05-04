3c336c648ad676f8b86e79c3bdc08ba2d3d5eb17
codingsixxx6 <riskianugerahf@gmail.com>
2026-05-04 12:44:29 +0700

    add function input user

--- hello.js
+++ hello.js
@@ -1,2 +1,23 @@
-console.log('Hello, AI');
+const readline = require('readline');
+
+function askUser(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
+
+async function main() {
+  const name = await askUser('Masukkan nama Anda: ');
+  console.log(`Halo, ${name}! Selamat belajar JavaScript.`);
+}
+
+main();
 
